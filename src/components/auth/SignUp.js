f7c76756e01af796e9cdf5839c9506062c3672a2
O-tega <tega.myview@gmail.com>
2023-01-19 23:19:58 +0100

    signup page implementation with formik

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,43 +1,151 @@
 import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Qrcode from "../../public/Qrcode1.jpg";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 const SignUp = () => {
-	const [email, setEmail] =
-		useState("");
-	const [password, setPassword] =
-		useState("");
-	const [firstName, setFirstName] =
-		useState("");
-	const [lastName, setLastName] =
-		useState("");
-	const [error, setError] =
-		useState("");
+	// <Formik
+	// 	initialValues={{
+	// 		email: "",
+	// 		password: "",
+	// 	}}
+	// 	validate={(values) => {
+	// 		const errors = {};
+	// 		if (!values.email) {
+	// 			errors.email = "Required";
+	// 		} else if (
+	// 			!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+	// 				values.email
+	// 			)
+	// 		) {
+	// 			errors.email =
+	// 				"Invalid email address";
+	// 		}
+	// 		return errors;
+	// 	}}
+	// 	onSubmit={(
+	// 		values,
+	// 		{ setSubmitting }
+	// 	) => {
+	// 		setTimeout(() => {
+	// 			alert(
+	// 				JSON.stringify(
+	// 					values,
+	// 					null,
+	// 					2
+	// 				)
+	// 			);
+	// 			setSubmitting(false);
+	// 		}, 400);
+	// 	}}>
+	// 	{({
+	// 		values,
+	// 		errors,
+	// 		touched,
+	// 		handleChange,
+	// 		handleBlur,
+	// 		handleSubmit,
+	// 		isSubmitting,
+	// 		/* and other goodies */
+	// 	}) => (
+	// 		<div className='flex justify-center mt-20'>
+	// 			<div className='w-[760px] md:grid grid-cols-2'>
+	// 				<div className='flex flex-col justify-center'>
+	// 					<p className='py-5 font-bold text-sky-700 text-lg pl-5'>
+	// 						SIGN-UP
+	// 					</p>
+	// 					<form
+	// 						onSubmit={handleSubmit}>
+	// 						<div className='flex flex-col px-5'>
+	// 							<div className='mb-3 w-full'>
+	// 								<TextField
+	// 									id='Firstname'
+	// 									label='First Name'
+	// 									variant='outlined'
+	// 									className='w-full'
+	// 									value={values.firstname
+	// 									}
+	// 									onChange={handleChange}
+	// 								/>
+	// 							</div>
+	// 							<div className='mb-3 w-full'>
+	// 								<TextField
+	// 									id='lastname'
+	// 									label='Last Name'
+	// 									variant='outlined'
+	// 									className='w-full'
+	// 									value={values.lastname
+	// 									}
+	// 									onChange={handleChange}
+	// 								/>
+	// 							</div>
+	// 							<div className='mb-3 w-full'>
+	// 								<TextField
+	// 									id='email'
+	// 									label='Email'
+	// 									variant='outlined'
+	// 									className='w-full'
+	// 									value={values.email}
+	// 									onChange={handleChange}
+	// 								/>
+	// 								{errors.email &&
+	// 									touched.email &&
+	// 									errors.email}
+	// 							</div>
+	// 							<div className='mb-3 w-full'>
+	// 								<TextField
+	// 									id='password'
+	// 									label='Password'
+	// 									variant='outlined'
+	// 									className='w-full'
+	// 									value={values.password
+	// 									}
+	// 									onChange={handleChange}
+	// 								/>
+	// 								{errors.password &&
+	// 									touched.password &&
+	// 									errors.password}
+	// 							</div>
+	// 							<div>
+	// 								<button
+	// 									type='submit'
+	// 									disabled={
+	// 										isSubmitting
+	// 									}
+	// 									className='rounded p-2 bg-sky-700 hover:bg-sky-900 w-full text-white text-lg font-medium'>
+	// 									Sign Up
+	// 								</button>
+	// 							</div>
+	// 						</div>
+	// 					</form>
+	// 				</div>
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		if (
-			email === "" &&
-			password === ""
-		) {
-			setError(
-				"please input email and password"
-			);
-		} else {
-			setEmail("");
-			setPassword("");
-			setFirstName("");
-			setLastName("");
-			setError("");
+	// 				{/* secod half of the grid */}
+	// 				<div className='hidden md:block'>
+	// 					<img
+	// 						src={Qrcode}
+	// 						alt='Qrcode image'
+	// 						className='object-cover h-full'
+	// 					/>
+	// 				</div>
+	// 			</div>
+	// 		</div>
+	// 	)}
+	// </Formik>;
+	const formik = useFormik({
+		initialValues: {
+			firstName: "",
+			lastName: "",
+			email: "",
+			password: "",
+		},
+		onSubmit: (values) => {
 			console.log(
-				firstName,
-				lastName,
-				email,
-				password
+				JSON.stringify(values)
 			);
-		}
-	};
-
+		},
+	});
 	return (
 		<div>
 			<div className='flex justify-center mt-20'>
@@ -46,69 +154,54 @@ const SignUp = () => {
 						<p className='py-5 font-bold text-sky-700 text-lg pl-5'>
 							SIGN-UP
 						</p>
-						<form
-							onSubmit={handleSubmit}>
+						<form onSubmit={formik.handleSubmit}>
 							<div className='flex flex-col px-5'>
 								<div className='mb-3 w-full'>
 									<TextField
-										id='Firstname'
+										id='firstName'
+										value={formik.values.firstName}
 										label='First Name'
 										variant='outlined'
 										className='w-full'
-										onChange={(e) =>
-											setFirstName(
-												e.target.value
-											)
-										}
+										onChange={formik.handleChange}
 									/>
 								</div>
 								<div className='mb-3 w-full'>
 									<TextField
-										id='lastname'
+										id='lastName'
+										value={formik.values.lastName}
 										label='Last Name'
 										variant='outlined'
 										className='w-full'
-										onChange={(e) =>
-											setLastName(
-												e.target.value
-											)
-										}
+										onChange={formik.handleChange}
 									/>
 								</div>
 								<div className='mb-3 w-full'>
 									<TextField
 										id='email'
+										value={formik.values.email}
 										label='Email'
 										variant='outlined'
 										className='w-full'
-										onChange={(e) =>
-											setEmail(
-												e.target.value
-											)
-										}
+										onChange={formik.handleChange}
 									/>
 								</div>
 								<div className='mb-3 w-full'>
 									<TextField
 										id='password'
+										value={formik.values.password}
 										label='Password'
 										variant='outlined'
 										className='w-full'
-										onChange={(e) =>
-											setPassword(
-												e.target.value
-											)
-										}
+										onChange={formik.handleChange}
 									/>
 								</div>
 								<div>
-									<button className='rounded p-2 bg-sky-700 hover:bg-sky-900 w-full text-white text-lg font-medium'>
+									<button type="submit" className='rounded p-2 bg-sky-700 hover:bg-sky-900 w-full text-white text-lg font-medium'>
 										Sign Up
 									</button>
 								</div>
-								<div className='text-xs text-red-500'>
-									{error}
-								</div>
+								<div className='text-xs text-red-500'></div>
 							</div>
 						</form>
 					</div>
@@ -126,5 +219,4 @@ const SignUp = () => {
 		</div>
 	);
 };
-
 export default SignUp;
