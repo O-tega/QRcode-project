e50fae4f468fa969111655dd3f128d7c6912583d
O-tega <tega.myview@gmail.com>
2023-01-20 02:44:00 +0100

    Modified yarn lock

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import TextField from "@mui/material/TextField";
 import Qrcode from "../../public/Qrcode1.jpg";
 import { useFormik } from "formik";
@@ -7,138 +7,11 @@ import * as yup from "yup";
 const validationSchema = yup.object({
 	firstName: yup.string().required("first name is required"),
 	lastName: yup.string().required("last name is required"),
-	email: yup.string().email("enter a valid email").required("Email is required")
+	email: yup.string().email("enter a valid email").required("Email is required"),
+	password: yup.string().required("Password is rewuired")
 })
 
 const SignUp = () => {
-	// <Formik
-	// 	initialValues={{
-	// 		email: "",
-	// 		password: "",
-	// 	}}
-	// 	validate={(values) => {
-	// 		const errors = {};
-	// 		if (!values.email) {
-	// 			errors.email = "Required";
-	// 		} else if (
-	// 			!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
-	// 				values.email
-	// 			)
-	// 		) {
-	// 			errors.email =
-	// 				"Invalid email address";
-	// 		}
-	// 		return errors;
-	// 	}}
-	// 	onSubmit={(
-	// 		values,
-	// 		{ setSubmitting }
-	// 	) => {
-	// 		setTimeout(() => {
-	// 			alert(
-	// 				JSON.stringify(
-	// 					values,
-	// 					null,
-	// 					2
-	// 				)
-	// 			);
-	// 			setSubmitting(false);
-	// 		}, 400);
-	// 	}}>
-	// 	{({
-	// 		values,
-	// 		errors,
-	// 		touched,
-	// 		handleChange,
-	// 		handleBlur,
-	// 		handleSubmit,
-	// 		isSubmitting,
-	// 		/* and other goodies */
-	// 	}) => (
-	// 		<div className='flex justify-center mt-20'>
-	// 			<div className='w-[760px] md:grid grid-cols-2'>
-	// 				<div className='flex flex-col justify-center'>
-	// 					<p className='py-5 font-bold text-sky-700 text-lg pl-5'>
-	// 						SIGN-UP
-	// 					</p>
-	// 					<form
-	// 						onSubmit={handleSubmit}>
-	// 						<div className='flex flex-col px-5'>
-	// 							<div className='mb-3 w-full'>
-	// 								<TextField
-	// 									id='Firstname'
-	// 									label='First Name'
-	// 									variant='outlined'
-	// 									className='w-full'
-	// 									value={values.firstname
-	// 									}
-	// 									onChange={handleChange}
-	// 								/>
-	// 							</div>
-	// 							<div className='mb-3 w-full'>
-	// 								<TextField
-	// 									id='lastname'
-	// 									label='Last Name'
-	// 									variant='outlined'
-	// 									className='w-full'
-	// 									value={values.lastname
-	// 									}
-	// 									onChange={handleChange}
-	// 								/>
-	// 							</div>
-	// 							<div className='mb-3 w-full'>
-	// 								<TextField
-	// 									id='email'
-	// 									label='Email'
-	// 									variant='outlined'
-	// 									className='w-full'
-	// 									value={values.email}
-	// 									onChange={handleChange}
-	// 								/>
-	// 								{errors.email &&
-	// 									touched.email &&
-	// 									errors.email}
-	// 							</div>
-	// 							<div className='mb-3 w-full'>
-	// 								<TextField
-	// 									id='password'
-	// 									label='Password'
-	// 									variant='outlined'
-	// 									className='w-full'
-	// 									value={values.password
-	// 									}
-	// 									onChange={handleChange}
-	// 								/>
-	// 								{errors.password &&
-	// 									touched.password &&
-	// 									errors.password}
-	// 							</div>
-	// 							<div>
-	// 								<button
-	// 									type='submit'
-	// 									disabled={
-	// 										isSubmitting
-	// 									}
-	// 									className='rounded p-2 bg-sky-700 hover:bg-sky-900 w-full text-white text-lg font-medium'>
-	// 									Sign Up
-	// 								</button>
-	// 							</div>
-	// 						</div>
-	// 					</form>
-	// 				</div>
-
-	// 				{/* secod half of the grid */}
-	// 				<div className='hidden md:block'>
-	// 					<img
-	// 						src={Qrcode}
-	// 						alt='Qrcode image'
-	// 						className='object-cover h-full'
-	// 					/>
-	// 				</div>
-	// 			</div>
-	// 		</div>
-	// 	)}
-	// </Formik>;
 	const formik = useFormik({
 		initialValues: {
 			firstName: "",
@@ -238,4 +111,5 @@ const SignUp = () => {
 		</div>
 	);
 };
+
 export default SignUp;
