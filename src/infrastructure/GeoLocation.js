4b922ac01662340c21e716057871df18c867a422
O-tega <tega.myview@gmail.com>
2023-01-30 17:17:27 +0100

    create location component to get location from user device

---
+++ src/infrastructure/GeoLocation.js
@@ -0,0 +1,43 @@
+import { useState, useEffect} from 'react';
+
+const GeoLocation =()=>{
+
+    const [location, setLocation]=useState({
+        loaded: false,
+        coordinates: {lat: "", lng: ""}
+    });
+
+    console.log(location)
+    const onSuccess = (location) =>{
+        setLocation({
+            loaded: true,
+            coordinates: {
+                lat: location.coords.latitude,
+                lng: location.coords.longitude
+            }
+        })
+    }
+
+    const onError = (error)=>{
+        setLocation({
+            loaded: true,
+            error
+        })
+    }
+
+    useEffect(()=>{
+        if (!("geolocation" in navigator)){
+            onError({
+                code: 0,
+                message: 'Geolocation is not supported'
+            })
+        }
+
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+    },[])
+
+    return location
+}
+
+export default GeoLocation;
